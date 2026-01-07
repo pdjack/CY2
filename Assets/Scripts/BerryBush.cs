cac319fbcd472dcb652bc448c400337277e87164
pdjack <doongji76@gmail.com>
2026-01-07 20:11:03 +0900

    Instanciate Berry
    random position

--- Assets/Scripts/BerryBush.cs
+++ Assets/Scripts/BerryBush.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,6 +8,7 @@ public class BerryBush : MonoBehaviour
     public GameObject berryPre;
 
     public int berryCount;
+    
     private void Start()
     {
         MakeBerry();
@@ -18,9 +20,9 @@ public class BerryBush : MonoBehaviour
         {
             float berryX = Random.Range(-0.25f, 0.25f);
             float berryY = Random.Range(-0.25f, 0.25f);
-        
+            Vector3 spawnPos = new Vector3(berryX, berryY, transform.position.z);
+            
             GameObject berry =  Instantiate(berryPre, gameObject.transform.position + new Vector3(berryX, berryY, 0), Quaternion.identity, transform);
-            berry.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground";
             
             berryCount++;
 
@@ -29,4 +31,24 @@ public class BerryBush : MonoBehaviour
         }
         
     }
+    
+    // void SpawnWithoutOverlap()
+    // {
+    //     Vector2 spawnPos;
+    //     bool canSpawn = false;
+    //
+    //     while (!canSpawn)
+    //     {
+    //         spawnPos = new Vector2(Random.Range(-spawnRange.x, spawnRange.x), Random.Range(-spawnRange.y, spawnRange.y));
+    //     
+    //         // 해당 위치에 'Obstacle' 레이어를 가진 콜라이더가 있는지 체크
+    //         Collider2D hit = Physics2D.OverlapCircle(spawnPos, prefabRadius);
+    //     
+    //         if (hit == null) {
+    //             Instantiate(prefab, spawnPos, Quaternion.identity);
+    //             canSpawn = true;
+    //         }
+    //     }
+
+    
 }
