3c340e87916e874aaa6ca5299ba3ffb580d8bf9b
pdjack <doongji76@gmail.com>
2026-01-15 14:12:37 +0900

    Instanciate Berry2

--- Assets/Scripts/BerryBush.cs
+++ Assets/Scripts/BerryBush.cs
@@ -13,42 +13,38 @@ public class BerryBush : MonoBehaviour
 {
     public GameObject berryPre;
 
-    public int berryCount;
+    private List<Berry> bPosListCopy;
+    private List<Berry> bPosList;
     
     private void Start()
     {
+        bPosList = new List<Berry> {new Berry{x = 0.3f, y =0.1f}, new Berry{x = -0.2f, y = 0.2f},new Berry{x = 0.1f, y = -0.3f}};
+        bPosListCopy = new List<Berry>(bPosList);
+        
         MakeBerry();
+        
     }
 
     private void MakeBerry()
     {
-        if (berryCount <= 6)
+        
+        if (bPosListCopy.Count != 0)
         {
-            List<Berry> bPosList = new List<Berry> {new Berry{x = 0.3f, y =0.1f}, new Berry{x = -0.2f, y = 0.2f},new Berry{x = 0.1f, y = -0.3f}};
-            List<Berry> bPosListCopy = new List<Berry>(bPosList);
-
-            
-            if (bPosListCopy.Count > 0)
+            if (bPosListCopy.Count == 0)
             {
-                int randomIndex = Random.Range(0, bPosListCopy.Count);
-            
-                float randomValueX = bPosListCopy[randomIndex].x;
-                float randomValueY = bPosListCopy[randomIndex].y;
-                
-                bPosListCopy.RemoveAt(randomIndex);
-                
-                //Debug.Log("random Index : " + randomIndex);
-                //Debug.Log("list count : " + bPosListCopy.Count);
-                //Debug.Log(" X값: " + randomValueX + " Y값: " + randomValueY);
+                bPosListCopy = new List<Berry>(bPosList);
+                Debug.Log("bPosListCopy 리스트 리셋");
             }
             
-            float berryX = Random.Range(-0.25f, 0.25f);
-            float berryY = Random.Range(-0.25f, 0.25f);
-            Vector3 spawnPos = new Vector3(berryX, berryY, transform.position.z);
+            int randomIndex = Random.Range(0, bPosListCopy.Count);
+            float randomValueX = bPosListCopy[randomIndex].x;
+            float randomValueY = bPosListCopy[randomIndex].y;
+            bPosListCopy.RemoveAt(randomIndex);
+            Debug.Log("randomValueX : " + randomValueX + "randomValueY : " + randomValueY);
             
-            GameObject berry =  Instantiate(berryPre, gameObject.transform.position + new Vector3(berryX, berryY, 0), Quaternion.identity, transform);
-            
-            berryCount++;
+            Vector3 spawnPos = gameObject.transform.position + new Vector3(randomValueX, randomValueY, transform.position.z);
+            Debug.Log("spawnPos : " + spawnPos);
+            GameObject berry =  Instantiate(berryPre, spawnPos, Quaternion.identity, transform);
 
             int makeTime =  Random.Range(3, 6);
             Invoke("MakeBerry", makeTime);
